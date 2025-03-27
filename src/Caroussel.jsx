ef9d53f982caa3e2b4838eaf8cbbfe8f4b260f85
LYJ <liladocdev@gmail.com>
2025-03-27 14:07:00 +0100

    add caroussel component

---
+++ src/Caroussel.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import styles from './Caroussel.module.css'
+import React from 'react'
+function Caroussel() {
+    return (
+        <div>
+            <h1>Caroussel</h1>
+        </div>
+    )
+}
+
+export default Caroussel
